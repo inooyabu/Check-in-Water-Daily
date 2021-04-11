7bc0c1f36a010a3d8a3ee8c59a932d2100f4fb04
tentiap <inooyabu@gmail.com>
2021-04-11 18:29:22 +0700

    Initial Commit

---
+++ Check-in Water Daily/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Check-in Water Daily
+//
+//  Created by Tenti Atika Putri on 11/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
